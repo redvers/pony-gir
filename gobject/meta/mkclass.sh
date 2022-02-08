eb1df02a1f04412835ddd3fd185d3b5ec1335a23
Redvers Davies <red@infect.me>
2022-02-08 18:00:50 -0500

    Reasonable initial run base GObject class (minus generics)

--- gobject/meta/mkclass.sh
+++ gobject/meta/mkclass.sh
@@ -12,9 +12,9 @@ if test -f "./active_methods/$3.xml"; then
 	saxon-he -xi active_methods/$3.xml ../../xsl/makeclassconstructors.xsl struct=$2 debug=0 >> ../../test/GObject/$2.pony
 fi
 
-#if test -f "./active_methods/$3.xml"; then
-##	saxon-he -xi active_methods/$3.xml ../../xsl/makemethods.xsl struct=$2 debug=0 >> ../../test/GObject/$2.pony
-#fi
+if test -f "./active_methods/$3.xml"; then
+	saxon-he -xi active_methods/$3.xml ../../xsl/makeclassmethods.xsl struct=$2 debug=0 >> ../../test/GObject/$2.pony
+fi
 
 #if test -f "./active_methods/$3.footer"; then
 ##	cat ./active_methods/$3.footer >> ../../test/GObject/$2.pony
