82a92a553437fd3a71c5364553ee6da7d596dc54
Redvers Davies <red@infect.me>
2022-02-08 23:49:34 -0500

    Remove FFI declarations from mkstruct

--- gobject/meta/mkstruct.sh
+++ gobject/meta/mkstruct.sh
@@ -1,7 +1,7 @@
 cp templates/use-libraries.txt ../../test/GObject/$2Struct.pony
-if test -f "./active_methods/$3.header"; then
-	cat ./active_methods/$3.header >> ../../test/GObject/$2Struct.pony
-fi
+#if test -f "./active_methods/$3.header"; then
+#	cat ./active_methods/$3.header >> ../../test/GObject/$2Struct.pony
+#fi
 #if test -f "./active_methods/$3.xml"; then
 #	saxon-he -xi active_methods/$3.xml ../../xsl/makeuse.xsl debug=0 >> ../../test/GObject/$2Struct.pony
 #fi
